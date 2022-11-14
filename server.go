5bb0a975f53226b613f4e13d8172876ead0846a0
ogb4n <ogb4n@protonmail.com>
2022-11-14 14:33:55 +0100

    last push

--- server.go
+++ server.go
@@ -11,8 +11,8 @@ type Data struct {
 }
 
 func main() {
-	fileServer := http.FileServer(http.Dir("./templates")) // New code
-	http.Handle("/", fileServer)                           // New code
+	fileServer := http.FileServer(http.Dir("./templates"))
+	http.Handle("/static/", http.StripPrefix("/static/", fileServer))
 
 	fmt.Printf("Starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
