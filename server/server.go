c48e23d52820a4ba0b7fe88a624d1e5b3555bdcc
ogb4n <ogb4n@protonmail.com>
2022-11-15 15:49:52 +0100

    push 16h

--- server/server.go
+++ server/server.go
@@ -5,46 +5,47 @@ import (
 	hc "hangmanweb/hangman_classic"
 	"html/template"
 	"net/http"
+	"os"
 )
 
-type hang struct {
-	Word   string
-	Letter string
+type dataSt struct {
+	Word       string
+	wordtofind string
+	usedletter []string
 }
+
+var data dataSt
 
 func main() {
 
-	hc.AccentChecker("eee")
-
 	http.HandleFunc("/", Handler) // Ici, quand on arrive sur la racine, on appelle la fonction Handler
-	//
+
 	fs := http.FileServer(http.Dir("./static/css"))
 	http.Handle("/css/", http.StripPrefix("/css/", fs))
-	//
+
 	fmt.Print("Le Serveur démarre sur le port 8080\n")
-	http.HandleFunc("/hangman", Handler) // Ici, on redirige vers /hangman pour effectuer les fonctions POST
+	http.HandleFunc("/hangman", Checker) // Ici, on redirige vers /hangman pour effectuer les fonctions POST
 	http.ListenAndServe(":8080", nil)
+	wordtofind = hc.CreateWord(os.Open())
 }
 
 func Handler(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("static/index.html"))
 
 	switch r.Method {
-	case "GET":
-		fmt.Println("GET")
 	case "POST": //
 		if err := r.ParseForm(); err != nil {
 			fmt.Fprintf(w, "ParseForm() err: %v", err)
 			return
 		}
 	}
-	variable := r.Form.Get("input")
-
-	data := hang{
-		Word:   "ça teste fort",
-		Letter: variable,
-	}
-
 	tmpl.Execute(w, data)
-	print(data.Letter, "\n")
+}
+
+func Checker(w http.ResponseWriter, r *http.Request) {
+	variable := r.FormValue("input")
+	println(variable)
+	hc.IsInputOk(variable, data.Word, data.wordtofind, &data.usedletter)
+	http.Redirect(w, r, "/", http.StatusSeeOther)
+
 }
